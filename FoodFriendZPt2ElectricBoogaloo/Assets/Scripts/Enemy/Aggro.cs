88d2f8e4ce5617a70bb6b6ba4b6ed68829735aa7
Buscems <46265895+Buscems@users.noreply.github.com>
2019-09-19 13:21:38 -0400

    Adding base enemy stuff
    I made a new scene for me to test stuff in and started making the aggro function it's own script as well as a generic base enemy script that can be applied to every enemy.

---
+++ FoodFriendZPt2ElectricBoogaloo/Assets/Scripts/Enemy/Aggro.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aggro : MonoBehaviour
+{
+
+    [Tooltip("How far away we want the enemy to be able to see the player")]
+    public float aggroRange;
+    [Tooltip("How long we want the enemy to be aggro after losing sight of the player")]
+    public float aggroTimerMax;
+
+    float aggroTimer;
+
+    bool aggro;
+
+    [Tooltip("This will be for how many players are in the game just in case we think about doing multiplayer")]
+    public Transform[] target;
+    Transform currentTarget;
+
+    [HideInInspector]
+    public Vector3 currentPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target[0] = GameObject.FindGameObjectWithTag("Player1").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AggroHandler();
+
+        Debug.Log(aggro);
+
+    }
+
+    void AggroHandler()
+    {
+
+        for (int i = 0; i < target.Length; i++)
+        {
+
+            if ((target[i].transform.position - currentPos).magnitude < aggroRange && !aggro)
+            {
+                aggro = true;
+                currentTarget = target[i];
+                aggroTimer = aggroTimerMax;
+            }
+            else
+            {
+                if (aggro)
+                {
+                    aggroTimer -= Time.deltaTime;
+                }
+            }
+        }
+
+        if (aggro)
+        {
+
+            if (aggroTimer <= 0)
+            {
+                aggroTimer = aggroTimerMax;
+                aggro = false;
+                currentTarget = null;
+            }
+
+        }
+
+    }
+
+}
