445d4f58015e0aaa1d4ad219edc3403430a2b354
SpanishMoses <50852159+SpanishMoses@users.noreply.github.com>
2019-09-23 15:49:26 -0400

    real enemy scripts
    this has all the enemy scripts that didnt get pushed before

---
+++ FoodFriendZPt2ElectricBoogaloo/Assets/Scripts/Enemy/DashEnemy.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashEnemy : MonoBehaviour
+{
+   public GameObject player;
+    public enemyMovement move;
+    private Vector3 playerPos;
+
+    public bool track;
+
+    public bool follow;
+
+    public float trackTime;
+    public float followTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine (Tracker());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (track == true){
+            playerPos = player.transform.position;
+            Debug.Log("track is true");
+        }
+        if (follow == true){
+            transform.position = Vector2.MoveTowards(transform.position, playerPos, move.speed * Time.deltaTime);
+            Debug.Log("follow is true");
+        }
+        /*
+        if (trackTimeStart >= maxTrackTime){
+            track = false;
+            follow = true;
+            Vector2 playerCurPos = player.transform.position;
+
+            if (followTimeStart >= stop){
+                move.speed = 0f;
+                follow = false;
+                track = true;
+                trackTimeStart = 0;
+                followTimeStart = 0;
+            }
+        }
+        */
+    }
+
+    IEnumerator Tracker(){
+        track = true;
+        yield return new WaitForSeconds(trackTime);
+        track = false;
+        StartCoroutine(Follow());
+    }
+
+    IEnumerator Follow(){
+        follow = true;
+        yield return new WaitForSeconds(followTime);
+        follow = false;
+        StartCoroutine(Tracker());
+    }
+}
---
+++ FoodFriendZPt2ElectricBoogaloo/Assets/Scripts/Enemy/KillSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillSpawner : MonoBehaviour
+{
+
+    public GameObject enemy;
+    public Rigidbody2D rb;
+    public bool isDead;
+    public float numEnemies;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isDead = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player"){
+            isDead = true;
+            for (int i = 0; i <= numEnemies; i++){
+                var temp = Instantiate(enemy, transform.position, transform.rotation);
+                temp.transform.position = new Vector3(Random.Range(temp.transform.position.x - 0.5f, temp.transform.position.x + 0.5f), Random.Range(temp.transform.position.y - 0.5f,temp.transform.position.y + 0.5f));
+            }
+            Destroy(gameObject);
+        }
+    }
+}
