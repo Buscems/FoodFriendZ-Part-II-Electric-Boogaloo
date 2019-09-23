031696602337ed94d693260bb3d2d2d26f915a16
Tim7596 <46041907+Tim7596@users.noreply.github.com>
2019-09-23 16:32:36 -0400

    Enemy Added and Adjustments

--- FoodFriendZPt2ElectricBoogaloo/Assets/Scripts/Enemy/MeleeEnemy.cs
+++ FoodFriendZPt2ElectricBoogaloo/Assets/Scripts/Enemy/MeleeEnemy.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 
 public class MeleeEnemy : MonoBehaviour
 {
+
+    BaseEnemy BaseEnemy;
+    MainPlayer MainPlayer;
+
     public GameObject Melee;
     public Vector2 startPos;
 
+    public int damage;
+
     void Start()
     {
+        BaseEnemy = this.GetComponent<BaseEnemy>();
+        MainPlayer = this.GetComponent<MainPlayer>();
         startPos = transform.position;
     }
 
     
     void Update()
     {
-        
+        if (BaseEnemy.aggroScript.aggro)
+        {
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            MainPlayer.health = MainPlayer.health - damage;
+        }
     }
 }
