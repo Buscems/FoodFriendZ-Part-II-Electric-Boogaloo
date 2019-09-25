85eeb375ac89fc85f89b8a0b00b79759e3d174e3
Buscems <46265895+Buscems@users.noreply.github.com>
2019-09-25 13:30:07 -0400

    Added percentage for item drop
    There is an item drop rate on the BaseEnemy that will make it so that they can drop items as well as an array where we can put the different types of items.

--- FoodFriendZPt2ElectricBoogaloo/Assets/Scripts/Enemy/BaseEnemy.cs
+++ FoodFriendZPt2ElectricBoogaloo/Assets/Scripts/Enemy/BaseEnemy.cs
@@ -12,12 +12,22 @@ public class BaseEnemy : MonoBehaviour
     [Tooltip("How fast we want the enemy to move")]
     public float speed;
 
-
     Animator anim;
 
     //variables for other enemy scripts to reference
     [HideInInspector]
     public Aggro aggroScript;
+
+    [Header("Item Drop Rate")]
+    public GameObject[] items;
+
+    [Tooltip("How often the enemy will actually drop an item \n \n The closer the number is to 0, the more likely it will be to drop an item")]
+    public float dropRate;
+
+    [Tooltip("How much money the enemy will drop when killed")]
+    public float money;
+
+    bool itemDrop;
 
     // Start is called before the first frame update
     public void Start()
@@ -25,6 +35,11 @@ public class BaseEnemy : MonoBehaviour
         if (GetComponent<Animator>() != null)
         {
             anim = GetComponent<Animator>();
+        }
+        itemDrop = false;
+        if(Random.Range(0, dropRate) == 0)
+        {
+            itemDrop = true;
         }
 
         //referencing any scripts that other enemies might need to
@@ -45,9 +60,14 @@ public class BaseEnemy : MonoBehaviour
 
     void Death()
     {
+        //This is the code for dropping an item
+        if (itemDrop)
+        {
+            Instantiate(items[Random.Range(0, items.Length)], transform.position, Quaternion.identity);
+        }
 
         //if there is an animation for death
-        if(anim != null)
+        if (anim != null)
         {
             //play the death animation
             anim.SetBool("Death", true);
