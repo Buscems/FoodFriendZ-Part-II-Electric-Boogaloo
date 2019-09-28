c1e9becf13b7159f362d00d138fec7da2a2b5a0b
Ganderman-Dan <46117179+Ganderman-Dan@users.noreply.github.com>
2019-09-28 17:16:43 -0400

    Added scene transitions and a place holder prefab for the exit. Right now it chooses any random scene, so i could take you back to the title screen technically

---
+++ FoodFriendZPt2ElectricBoogaloo/Assets/Scripts/ExitLogic.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitLogic : MonoBehaviour
+{
+    public ScreenTransition st;
+
+    public bool pickRandomScene;
+    private string sceneToGoTo;
+    private int randomSceneToGoTo;
+
+
+    void Start()
+    {
+        print("num scenes: " + SceneManager.sceneCountInBuildSettings);
+    }
+
+    IEnumerator StartTransition()
+    {
+        st.FadeOut();
+        yield return new WaitForSeconds(st.fadeLength);
+        if (pickRandomScene)
+        {
+            int randomSceneToGoTo = Random.Range(0, SceneManager.sceneCountInBuildSettings);
+            SceneManager.LoadScene(randomSceneToGoTo);
+        }
+        else
+        {
+            SceneManager.LoadScene(sceneToGoTo);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player1")
+        {
+            StartCoroutine(StartTransition());
+        }
+    }
+}
