07968604309dd40ac36453118088ea73a48a2de5
SpanishMoses <50852159+SpanishMoses@users.noreply.github.com>
2019-09-30 16:36:36 -0400

    mole and worm enemy
    Created mole enemy and started the worm enemy

---
+++ FoodFriendZPt2ElectricBoogaloo/Assets/Scripts/Enemy/WormEnemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WormEnemy : MonoBehaviour
+{
+    public BaseEnemy baseEnemy;
+
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        baseEnemy = GetComponent<BaseEnemy>();
+        rb.AddForce(new Vector2(baseEnemy.speed, baseEnemy.speed));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+}
