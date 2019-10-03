f88dd2938c91206570d7a17ebf45ec03f6e798af
SpanishMoses <50852159+SpanishMoses@users.noreply.github.com>
2019-10-03 16:49:56 -0400

    worm enemy
    Worm enemy completed

---
+++ FoodFriendZPt2ElectricBoogaloo/Assets/Scripts/Enemy/OtherWormEnemy.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherWormEnemy : MonoBehaviour
+{
+    //code from https://answers.unity.com/questions/1359733/moving-an-enemy-randomly.html
+
+    private float latestDirectionChangeTime;
+    private readonly float directionChangeTime = 1f;
+    private float characterVelocity = 5f;
+    private Vector2 movementDirection;
+    private Vector2 movementPerSecond;
+
+    public CircleCollider2D alert;
+
+    void Start()
+    {
+        latestDirectionChangeTime = 0f;
+        calcuateNewMovementVector();
+    }
+
+    void calcuateNewMovementVector()
+    {
+        //create a random direction vector with the magnitude of 1, later multiply it with the velocity of the enemy
+        movementDirection = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)).normalized;
+        movementPerSecond = movementDirection * characterVelocity;
+    }
+
+    void Update()
+    {
+        //if the changeTime was reached, calculate a new movement vector
+        if (Time.time - latestDirectionChangeTime > directionChangeTime)
+        {
+            latestDirectionChangeTime = Time.time;
+            calcuateNewMovementVector();
+        }
+
+        //move enemy: 
+        transform.position = new Vector2(transform.position.x + (movementPerSecond.x * Time.deltaTime),
+        transform.position.y + (movementPerSecond.y * Time.deltaTime));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Wall"){
+            Debug.Log("CHANGE");
+            calcuateNewMovementVector();
+        }
+    }
+}
+
