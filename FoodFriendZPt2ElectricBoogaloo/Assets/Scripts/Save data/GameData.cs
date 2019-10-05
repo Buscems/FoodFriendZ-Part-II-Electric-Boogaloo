ea6b9b30c304e5c4249610bedd98192315a7fb66
Ganderman-Dan <46117179+Ganderman-Dan@users.noreply.github.com>
2019-10-04 20:19:58 -0400

    Its completely empty, but there is now a save file that we can read and write to, i just need pat to send me al the information i need to input that data into the save file

---
+++ FoodFriendZPt2ElectricBoogaloo/Assets/Scripts/Save data/GameData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameData
+{
+    //                           0                       1                        2                    3               4            5                 6                     7
+    //game stats ->    [ Total money earned ] [ Total bullets Fired  ] [ Total enemies killed ] [ xxxxxxxxxxx ] [xxxxxxxxxx] [xxxxxxxxxx] [xxxxxxxxxxxxxxxxxxx] [xxxxxxxxxxxxxxxxxxxx] 
+
+    public string[] gameStats { set; get; }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void SetVariables(string[] _gameStats)
+    {
+        gameStats = _gameStats;
+    }
+}
