4ffe5197d44199d3fafed61f791e16864bac3667
SpanishMoses <50852159+SpanishMoses@users.noreply.github.com>
2019-10-07 08:29:05 -0400

    water enemy done
    Enemy that leaves a trail tht player must avoid

--- FoodFriendZPt2ElectricBoogaloo/Assets/Scripts/Enemy/water.cs
+++ FoodFriendZPt2ElectricBoogaloo/Assets/Scripts/Enemy/water.cs
@@ -4,26 +4,23 @@ using UnityEngine;
 
 public class water : MonoBehaviour
 {
-
-    public MainPlayer player;
+    public GameObject waterObj;
+    public float destroyTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GetComponent<MainPlayer>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Destroy(gameObject, destroyTime);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Player1"){
-            player = gameObject.GetComponent<MainPlayer>();
 
         }
-    }
 }
