f5bb3ccaae8ebd97941ae8b6ec9f5873847aa3b1
SpanishMoses <50852159+SpanishMoses@users.noreply.github.com>
2019-10-07 13:46:09 -0400

    more enemies
    Two enemies started but not finished :(

---
+++ FoodFriendZPt2ElectricBoogaloo/Assets/Scripts/Enemy/KoopaLikeEnemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KoopaLikeEnemy : MonoBehaviour
+{
+    private Vector2 movementDirection;
+    private Vector2 movementPerSecond;
+    private float characterVelocity = 5f;
+
+    public bool isDizzy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementPerSecond = movementDirection * characterVelocity;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (isDizzy && collision.gameObject.tag == "Player1"){
+            transform.position = new Vector2(transform.position.x + (movementPerSecond.x * Time.deltaTime),
+        transform.position.y + (movementPerSecond.y * Time.deltaTime));
+        }
+    }
+}
