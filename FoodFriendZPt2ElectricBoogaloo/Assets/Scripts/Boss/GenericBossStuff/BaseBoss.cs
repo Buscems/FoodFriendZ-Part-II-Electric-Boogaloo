771af16a5f889cc55572d59a7326450a9d920b35
Buscems <46265895+Buscems@users.noreply.github.com>
2019-10-10 13:47:07 -0400

    Made an example level for Georgia and Nick Frank. Also did some boss stuff.

---
+++ FoodFriendZPt2ElectricBoogaloo/Assets/Scripts/Boss/GenericBossStuff/BaseBoss.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseBoss : MonoBehaviour
+{
+
+    [Tooltip("This is how many doors need to be opened after the boss dies")]
+    public GameObject[] doors;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Death()
+    {
+        for(int i = 0; i < doors.Length; i++)
+        {
+            doors[i].SetActive(false);
+        }
+    }
+
+}
