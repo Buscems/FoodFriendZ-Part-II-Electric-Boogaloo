b607523daf22361f904001525a2afcaf11131f43
Buscems <46265895+Buscems@users.noreply.github.com>
2019-10-14 12:16:42 -0400

    Update MainPlayer.cs

--- FoodFriendZPt2ElectricBoogaloo/Assets/Scripts/Player/MainPlayer.cs
+++ FoodFriendZPt2ElectricBoogaloo/Assets/Scripts/Player/MainPlayer.cs
@@ -12,6 +12,13 @@ public class MainPlayer : MonoBehaviour
     [HideInInspector]
     public int currency;
 
+    [Header("Audio")]
+    public AudioSource audioSource;
+    [Tooltip("These are the players sound clips")]
+    /*
+     * 1 = deathSound
+     */
+    public AudioClip[] clips;
 
     //the following is in order to use rewired
     [Tooltip("Reference for using rewired")]
@@ -190,6 +197,12 @@ public class MainPlayer : MonoBehaviour
             {
                 currentChest.OpenChest();
             }
+
+        }
+
+        if(health <= 0)
+        {
+            //death happens here
 
         }
 
