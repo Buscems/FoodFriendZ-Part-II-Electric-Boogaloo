7c530ec441cafde239b260838a5fb2e172943658
Tim7596 <46041907+Tim7596@users.noreply.github.com>
2019-10-19 18:01:36 -0400

    Shrieker added

---
+++ FoodFriendZPt2ElectricBoogaloo/Assets/Scripts/Enemy/Shreiker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shreiker : MonoBehaviour
+{
+    Aggro agrro;
+    BaseEnemy baseEnemy;
+    private GameObject[] nearbyEnemy;
+    [SerializeField] private float shriekerRange;
+
+    void Start()
+    {
+        agrro = GetComponent<Aggro>();
+        baseEnemy = GetComponent<BaseEnemy>();
+    }
+
+    
+    void Update()
+    {
+        if (baseEnemy.aggroScript.aggro)
+        {
+
+            Alert(transform.position, shriekerRange);
+            
+        }
+    }
+
+    void Alert(Vector3 center, float radius)
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(center, radius);
+        int i = 0;
+        while (i < hitColliders.Length)
+        {
+
+            hitColliders[i].GetComponent<BaseEnemy>().aggroScript.aggroRange = baseEnemy.aggroScript.aggroRange * 2;
+            i++;
+        }
+    }
+}
