526deaf7f67b8d1d8da2f06c4f8cd36994e4fa5e
Ganderman-Dan <46117179+Ganderman-Dan@users.noreply.github.com>
2019-10-19 17:39:11 -0400

    you cant spam melee attacks anymore

--- FoodFriendZPt2ElectricBoogaloo/Assets/Scripts/Player/GoToNextLevel.cs
+++ FoodFriendZPt2ElectricBoogaloo/Assets/Scripts/Player/GoToNextLevel.cs
@@ -39,12 +39,16 @@ public class GoToNextLevel : MonoBehaviour
             newNumber = Random.Range(0, levels.Length);
         }
 
-        Destroy(currentScene);
-        currentScene = Instantiate(levels[newNumber], Vector3.zero, Quaternion.identity);
-        Vector3 spawnPoint = GameObject.Find("SPAWNPOINT").transform.position;
-        print(spawnPoint);
-        player.transform.position = spawnPoint;
-        camera.transform.position = spawnPoint;
+        try
+        {
+            Destroy(currentScene);
+            currentScene = Instantiate(levels[newNumber], Vector3.zero, Quaternion.identity);
+            Vector3 spawnPoint = GameObject.Find("SPAWNPOINT").transform.position;
+            print(spawnPoint);
+            player.transform.position = spawnPoint;
+            camera.transform.position = spawnPoint;
+        }
+        catch { }
 
     }
 }
