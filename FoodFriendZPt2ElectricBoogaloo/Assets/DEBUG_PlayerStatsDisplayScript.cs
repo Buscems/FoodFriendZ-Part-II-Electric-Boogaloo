165404bb59136094cc5c2a12f3ebf22c2592799d
Jesse Taylor <55162534+Shmooplely@users.noreply.github.com>
2019-10-21 16:36:11 -0400

    item

---
+++ FoodFriendZPt2ElectricBoogaloo/Assets/DEBUG_PlayerStatsDisplayScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DEBUG_PlayerStatsDisplayScript : MonoBehaviour
+{
+    [Header("Scripts")]
+    public MainPlayer pScript;
+
+    [Header("Text")]
+    public TextMeshProUGUI mspeedDisp;
+    public TextMeshProUGUI aspeedDisp;
+    public TextMeshProUGUI ApowDisp;
+
+    //variables
+    float mspeedMult;
+    float aspeedMult;
+    float apowMult;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
