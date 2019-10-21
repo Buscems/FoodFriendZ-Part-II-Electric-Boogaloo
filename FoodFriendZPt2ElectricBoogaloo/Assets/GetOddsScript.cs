236b50a5d714d321a43478cb25d1ef4c6686965a
Jesse Taylor <55162534+Shmooplely@users.noreply.github.com>
2019-10-21 14:25:08 -0400

    Created Odds Generator

---
+++ FoodFriendZPt2ElectricBoogaloo/Assets/GetOddsScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetOddsScript : MonoBehaviour
+{
+
+    public bool getStunOdds(float chance)
+    {
+        bool isSuccessful = false;
+
+        //rng for deteriminging success
+
+
+        if (Random.Range(0.0f, 1.0f) <= chance)
+        {
+            isSuccessful = true;
+        }
+        return isSuccessful;
+    }
+
+
+}
