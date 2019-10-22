54decb6b673368a01f66f8b3927401a08e34972e
Jesse Taylor <55162534+Shmooplely@users.noreply.github.com>
2019-10-21 20:31:51 -0400

    Added Debug Display for stats

--- FoodFriendZPt2ElectricBoogaloo/Assets/DEBUG_PlayerStatsDisplayScript.cs
+++ FoodFriendZPt2ElectricBoogaloo/Assets/DEBUG_PlayerStatsDisplayScript.cs
@@ -10,14 +10,9 @@ public class DEBUG_PlayerStatsDisplayScript : MonoBehaviour
     public MainPlayer pScript;
 
     [Header("Text")]
-    public TextMeshProUGUI mspeedDisp;
-    public TextMeshProUGUI aspeedDisp;
-    public TextMeshProUGUI ApowDisp;
-
-    //variables
-    float mspeedMult;
-    float aspeedMult;
-    float apowMult;
+    public TextMeshProUGUI mSpeedDisp;
+    public TextMeshProUGUI aSpeedDisp;
+    public TextMeshProUGUI aPowDisp;
 
     void Start()
     {
@@ -27,5 +22,8 @@ public class DEBUG_PlayerStatsDisplayScript : MonoBehaviour
     void Update()
     {
         //update text
+        mSpeedDisp.text = pScript.speedMultiplier.ToString("00.00");
+        aSpeedDisp.text = pScript.attackSpeedMultiplier.ToString("00.00");
+        aPowDisp.text = pScript.maxDamageMultiplier.ToString("00.00");
     }
 }
