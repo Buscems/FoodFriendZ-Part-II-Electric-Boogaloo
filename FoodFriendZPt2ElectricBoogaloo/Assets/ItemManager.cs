52c1595170247a5bc5b709be30ba9a7cc42d1e10
Jesse Taylor <55162534+Shmooplely@users.noreply.github.com>
2019-10-28 16:09:51 -0400

    Implemented CoolDownTimer

--- FoodFriendZPt2ElectricBoogaloo/Assets/ItemManager.cs
+++ FoodFriendZPt2ElectricBoogaloo/Assets/ItemManager.cs
@@ -4,29 +4,62 @@ using UnityEngine;
 
 public class ItemManager : MonoBehaviour
 {
-
+    //[ALL VARIABLES
     private MainPlayer mp;
+    public PowerUps PowerUpScript;
     public GameObject item = null;
+
+    //timer stuff
     public float curTimer;
 
+
+    //[START]
     void Start()
     {
         mp = GetComponent<MainPlayer>();
+
+        //FOR DEBUGGING
+        if (item != null)
+        {
+            PowerUpScript = item.GetComponent<PowerUps>();
+        }
     }
 
     void Update()
     {
         //check if player can use the item
-        if (Input.GetKeyDown(KeyCode.E) && item != null)
+        //[use ACTIVE ITEM button]
+        if (Input.GetKeyDown(KeyCode.E) && item != null && curTimer == 0)
         {
             UseItem();
         }
+        else if (curTimer > 0)
+        {
+            print("Cannot Use Item Yet!!");
+        }
+
+        //cooldown timer check
+        if (curTimer > 0)
+        {
+            //trickle timer down
+            curTimer -= Time.deltaTime;
+
+            //when timer hits
+            if (curTimer <= 0)
+            {
+                //resets timer
+                curTimer = 0;
+            }
+        }
     }
 
+    //[METHODS]
     private void UseItem()
     {
         Instantiate(item, transform.position, Quaternion.Euler(mp.attackDirection.transform.eulerAngles.x, mp.attackDirection.transform.eulerAngles.y, mp.attackDirection.transform.eulerAngles.z));
-        //item = null;
+
+        //set timer
+        curTimer = PowerUpScript.maxCoolDownDuration;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -37,6 +70,9 @@ public class ItemManager : MonoBehaviour
             {
                 item = other.gameObject;
                 Destroy(other.gameObject);
+
+                //retrieves the instance of the power up script on the power up grabbed
+                PowerUpScript = other.gameObject.GetComponent<PowerUps>();
             }
         }
     }
