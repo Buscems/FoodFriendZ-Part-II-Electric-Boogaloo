5daa5071d3b3cab2e5efe21b7c202eb3a3fa43e6
SpanishMoses <50852159+SpanishMoses@users.noreply.github.com>
2019-10-29 01:58:13 -0400

    koopa enemy done
    Koopa enemy is finished and tested. Needs minor tweeks but the main function of it works

--- FoodFriendZPt2ElectricBoogaloo/Assets/Scripts/Enemy/KoopaLikeEnemy.cs
+++ FoodFriendZPt2ElectricBoogaloo/Assets/Scripts/Enemy/KoopaLikeEnemy.cs
@@ -4,51 +4,90 @@ using UnityEngine;
 
 public class KoopaLikeEnemy : MonoBehaviour
 {
-    private Vector2 movementDirection;
-    private Vector2 movementPerSecond;
-    private float characterVelocity = 5f;
+    private Vector3 playerPos;
+    Vector3 dir;
 
-    public bool notDizzy;
-    public bool isDizzy;
-    public bool isSpinning;
+    public bool awake;
+    public bool dizzy;
+    public bool spinning;
 
-    public BaseEnemy enemy;
-    public Tracking track;
+    public float timeForDizzy;
+    public float timeForSpinning;
+
+    BaseEnemy baseEnemy;
+    public PathfindingAI path;
+
+    public Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        notDizzy = true;
-        enemy = GetComponent<BaseEnemy>();
-        track = GetComponent<Tracking>();
+        awake = true;
+        baseEnemy = GetComponent<BaseEnemy>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (notDizzy == true && enemy.aggroScript.aggro){
-            
+        if (awake == true) {
+            path.enabled = true;
+            baseEnemy.walkIntoDamage = 1;
+            if (baseEnemy.aggroScript.aggro == true) {
+                playerPos = baseEnemy.aggroScript.currentTarget.transform.position;
+            }
         }
-        
 
-        if (isSpinning == true)
-        {
-            movementPerSecond = movementDirection * characterVelocity;
-            transform.position = new Vector2(transform.position.x + (movementPerSecond.x * Time.deltaTime),
-        transform.position.y + (movementPerSecond.y * Time.deltaTime));
+        if (dizzy == true) {
+            path.enabled = false;
+            baseEnemy.walkIntoDamage = 0;
+        }
+
+        if (spinning == true) {
+            dizzy = false;
+        }
+
+        if (baseEnemy.health <= 2) {
+            StartCoroutine(dizzyTime());
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+
+        if (other.gameObject.CompareTag("Player1") && dizzy == true) {
+            StopAllCoroutines();
+            baseEnemy.health += 4;
+            spinning = true;
+            Vector2 wallNormal = other.contacts[0].normal;
+            dir = Vector2.Reflect(-rb.velocity, wallNormal).normalized;
+
+            rb.velocity = dir * baseEnemy.speed * 4;
+        }
+
+        if (other.gameObject.CompareTag("TilesHere") && spinning == true){
+            StartCoroutine(spinningTime());
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (notDizzy && collision.gameObject.tag == "Slash"){
-            notDizzy = false;
-            enemy.aggroScript.aggro = false;
-            track.StopAllCoroutines();
-            track.track = false;
-            track.follow = false;
-            isDizzy = true;
+        if (collision.gameObject.tag == "Enemy" && spinning == true){
+            Destroy(collision.gameObject);
         }
-        
+    }
+
+    IEnumerator dizzyTime() {
+        awake = false;
+        dizzy = true;
+        yield return new WaitForSeconds(timeForDizzy);
+        dizzy = false;
+        awake = true;
+    }
+
+    IEnumerator spinningTime(){
+        yield return new WaitForSeconds(timeForSpinning);
+        spinning = false;
+        awake = true;
     }
 }
