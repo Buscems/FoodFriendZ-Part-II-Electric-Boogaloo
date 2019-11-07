09d3c8a9aadbe135d1ad8622525d6415c0cb024e
SpanishMoses <50852159+SpanishMoses@users.noreply.github.com>
2019-11-07 16:40:32 -0500

    another enemy started
    started another enemy even though i shouldnt have

---
+++ FoodFriendZPt2ElectricBoogaloo/Assets/Scripts/Enemy/revolvingEnemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class revolvingEnemy : MonoBehaviour
+{ 
+    public GameObject[] projectiles;
+    public float numProjectiles;
+
+    public float rangeOfSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i <= numProjectiles; i++)
+        {
+            int rand = Random.Range(0, 1);
+            var temp = Instantiate(projectiles[rand], transform.position, transform.rotation);
+            temp.transform.position = new Vector3(Random.Range(temp.transform.position.x - rangeOfSpawn, temp.transform.position.x + rangeOfSpawn), Random.Range(temp.transform.position.y - rangeOfSpawn, temp.transform.position.y + rangeOfSpawn));
+        }
+    }
+}
