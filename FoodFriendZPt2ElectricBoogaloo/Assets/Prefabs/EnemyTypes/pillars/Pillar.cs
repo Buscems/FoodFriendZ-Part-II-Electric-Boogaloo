7c0ee63f07ca3bbe90ed2a73b0dbd30e83570492
SpanishMoses <50852159+SpanishMoses@users.noreply.github.com>
2019-11-12 23:28:19 -0500

    pillar enemy finished
    Finished the pillar enemy to a point where its usable

--- FoodFriendZPt2ElectricBoogaloo/Assets/Prefabs/EnemyTypes/pillars/Pillar.cs
+++ FoodFriendZPt2ElectricBoogaloo/Assets/Prefabs/EnemyTypes/pillars/Pillar.cs
@@ -6,17 +6,14 @@ public class Pillar : MonoBehaviour
 {
     public PillarEnemy pillar;
     public float speed;
+    Vector2 velocity;
     Rigidbody2D rb;
-    Vector3 velocityX;
-    Vector3 velocityY;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         pillar = GetComponent<PillarEnemy>();
-        velocityX = transform.up * speed;
-        velocityY = transform.right * speed;
     }
 
     // Update is called once per frame
@@ -27,23 +24,13 @@ public class Pillar : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (pillar.spawnDown == true)
-        {
-            rb.MovePosition(Vector2.down * speed * Time.deltaTime);
-        }
+        Vector3 pos = transform.position;
 
-        if (pillar.spawnRight == true)
-        {
-            rb.MovePosition(Vector2.left * speed * Time.deltaTime);
-        }
-        if (pillar.spawnUp == true)
-        {
-            rb.MovePosition(Vector2.up * speed * Time.deltaTime);
-        }
-        if (pillar.spawnRight == true)
-        {
-            rb.MovePosition(Vector2.right * speed * Time.deltaTime);
-        }
+        Vector3 velocity = new Vector3(-speed * Time.deltaTime, 0);
+
+        pos += transform.rotation * velocity;
+
+        transform.position = pos;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
