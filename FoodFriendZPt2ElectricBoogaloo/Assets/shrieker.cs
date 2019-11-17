6586e6319bcb29f7be35f6814781d865dc6f20f2
SpanishMoses <50852159+SpanishMoses@users.noreply.github.com>
2019-11-16 22:33:45 -0500

    enemy currency value
    gave enemies currency values and also started new enemy that may or may not be ready for monday

---
+++ FoodFriendZPt2ElectricBoogaloo/Assets/shrieker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shrieker : MonoBehaviour
+{
+
+    BaseEnemy baseEnemy;
+    public CircleCollider2D alert;
+    PathfindingAI path;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        baseEnemy = GetComponent<BaseEnemy>();
+        path = GetComponent<PathfindingAI>();
+        path.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (baseEnemy.aggroScript.aggro == true){
+            alert.radius = 5f;
+        }
+    }
+}
