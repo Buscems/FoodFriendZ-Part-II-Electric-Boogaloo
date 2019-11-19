a6d5aadf85e4ab7e11f78c0e981fbe17cec3c5af
SpanishMoses <50852159+SpanishMoses@users.noreply.github.com>
2019-11-18 23:08:32 -0500

    revolving enemy fixes started
    started to fix revolving enemy but got tired so this is what i got for now

--- FoodFriendZPt2ElectricBoogaloo/Assets/Scripts/Enemy/revolvingBullet.cs
+++ FoodFriendZPt2ElectricBoogaloo/Assets/Scripts/Enemy/revolvingBullet.cs
@@ -7,19 +7,36 @@ public class revolvingBullet : MonoBehaviour
     public GameObject proj;
     public Transform revolveAround;
     public float speed;
+    public Vector3 velocity;
+    BaseEnemy baseEnemy;
+
+    Rigidbody2D rb;
 
     private Vector3 zAxis = new Vector3(0, 0, 1);
+
+    public bool revolve;
+    public bool shoot;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        baseEnemy = GetComponent<BaseEnemy>();
+        rb = GetComponent<Rigidbody2D>();
+        revolve = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.RotateAround(revolveAround.position, zAxis, speed);
+        if (revolve == true)
+        {
+            transform.RotateAround(revolveAround.position, zAxis, speed);
+        }
+
+        /*if (baseEnemy.aggroScript.aggro == true){
+            revolve = false;
+            rb.MovePosition(transform.position + velocity * speed * Time.deltaTime);
+        }*/
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
