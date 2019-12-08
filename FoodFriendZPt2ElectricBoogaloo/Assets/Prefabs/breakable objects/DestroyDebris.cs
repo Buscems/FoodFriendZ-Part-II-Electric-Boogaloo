0b69e2a8bf11f96b87f1d6b6fb6c3eae43f5213b
kirenasura <31701097+kirenasura@users.noreply.github.com>
2019-12-08 17:56:37 -0500

    added debris sounds

--- FoodFriendZPt2ElectricBoogaloo/Assets/Prefabs/breakable objects/DestroyDebris.cs
+++ FoodFriendZPt2ElectricBoogaloo/Assets/Prefabs/breakable objects/DestroyDebris.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class DestroyDebris : MonoBehaviour
 {
+    public AudioSource breakableSound;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playASound();
     }
 
     // Update is called once per frame
     void Update()
     {
         Destroy(gameObject, 3);
+    }
+
+    void playASound()
+    {
+        breakableSound.Play();
     }
 }
